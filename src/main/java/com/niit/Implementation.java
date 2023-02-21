515062a24e16891b3ded1e6ff6f0c99b28fe3631
SHUBHAM <shubhamtiwary50@gmail.com>
2023-02-21 15:11:07 +0530

    Refactored the Implementation class according to the requirements of the Question. Able to display movie name.

---
+++ src/main/java/com/niit/Implementation.java
@@ -0,0 +1,17 @@
+package com.niit;
+
+import com.niit.config.BeanConfig;
+import com.niit.domain.Movie;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+/**
+ * Hello world!
+ */
+public class Implementation {
+    public static void main(String[] args) {
+      ApplicationContext applicationContext= new AnnotationConfigApplicationContext(BeanConfig.class);
+      Movie movie = applicationContext.getBean(Movie.class);
+        System.out.println(movie);
+    }
+}
