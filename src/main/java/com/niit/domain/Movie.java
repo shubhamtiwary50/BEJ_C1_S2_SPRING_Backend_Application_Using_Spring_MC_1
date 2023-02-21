7187ef180e8acb75c32abf3736f2f2a0ff4c3d49
SHUBHAM <shubhamtiwary50@gmail.com>
2023-02-21 15:04:49 +0530

    Added getter, setters, hashcode, equals and tostring()

--- src/main/java/com/niit/domain/Movie.java
+++ src/main/java/com/niit/domain/Movie.java
@@ -1,5 +1,7 @@
 package com.niit.domain;
 
+import java.util.Objects;
+
 public class Movie {
     private String movieName;
     private String movieRating;
@@ -14,5 +16,60 @@ public class Movie {
         this.movieRating = movieRating;
         this.movieBoxOffice = movieBoxOffice;
         this.movieId = movieId;
+    }
+
+    public String getMovieName() {
+        return movieName;
+    }
+
+    public void setMovieName(String movieName) {
+        this.movieName = movieName;
+    }
+
+    public String getMovieRating() {
+        return movieRating;
+    }
+
+    public void setMovieRating(String movieRating) {
+        this.movieRating = movieRating;
+    }
+
+    public String getMovieBoxOffice() {
+        return movieBoxOffice;
+    }
+
+    public void setMovieBoxOffice(String movieBoxOffice) {
+        this.movieBoxOffice = movieBoxOffice;
+    }
+
+    public int getMovieId() {
+        return movieId;
+    }
+
+    public void setMovieId(int movieId) {
+        this.movieId = movieId;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Movie)) return false;
+        Movie movie = (Movie) o;
+        return getMovieId() == movie.getMovieId() && Objects.equals(getMovieName(), movie.getMovieName()) && Objects.equals(getMovieRating(), movie.getMovieRating()) && Objects.equals(getMovieBoxOffice(), movie.getMovieBoxOffice());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getMovieName(), getMovieRating(), getMovieBoxOffice(), getMovieId());
+    }
+
+    @Override
+    public String toString() {
+        return "Movie{" +
+                "movieName='" + movieName + '\'' +
+                ", movieRating='" + movieRating + '\'' +
+                ", movieBoxOffice='" + movieBoxOffice + '\'' +
+                ", movieId=" + movieId +
+                '}';
     }
 }
