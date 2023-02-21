b63dca0f81d116f003ce00e0e7ba5820b7ec2f1c
SHUBHAM <shubhamtiwary50@gmail.com>
2023-02-21 14:58:41 +0530

    added constructors for domain class movie

--- src/main/java/com/niit/domain/Movie.java
+++ src/main/java/com/niit/domain/Movie.java
@@ -1,4 +1,18 @@
 package com.niit.domain;
 
 public class Movie {
+    private String movieName;
+    private String movieRating;
+    private String movieBoxOffice;
+    private int movieId;
+
+    public Movie() {
+    }
+
+    public Movie(String movieName, String movieRating, String movieBoxOffice, int movieId) {
+        this.movieName = movieName;
+        this.movieRating = movieRating;
+        this.movieBoxOffice = movieBoxOffice;
+        this.movieId = movieId;
+    }
 }
